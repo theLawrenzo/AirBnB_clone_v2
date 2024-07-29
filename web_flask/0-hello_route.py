071007038dcaf639bbf4fcb72c6b0db8a4567425
theLawrenzo <thelawrenzo1@gmail.com>
2024-07-29 04:24:58 +0000

    Flask web application home page uploaded

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+# This is a script that starts a flask web application
+'''
+A flask web application that runs on all available network
+interface on machine it is run on
+'''
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/", strict_slashes=False)
+def home():
+	return "Hello HBNB!"
+
+if __name__ == "__main__":
+	app.run(host='0.0.0.0', port=5000, debug=True)
---
+++ web_flask/README.md
@@ -0,0 +1,11 @@
+ALX SE
+PROJECT : AirBnB clone - Web framework
+
+TASKS:
+
+0. Hello Flask!
+- Write a script that starts a flask web application
+	. Application must be listening on 0.0.0.0 port 5000
+
+1. HBNB
+- Write a script that starts a flask web application
