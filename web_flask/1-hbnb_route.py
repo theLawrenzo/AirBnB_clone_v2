8537afea38d36528bde842d21d7166f5acf76639
theLawrenzo <thelawrenzo1@gmail.com>
2024-07-29 04:40:58 +0000

    Script that runs a Flask web application

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# This script contians a flask web application
+
+'Flask web application that run on all computer in the same network'
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+	return "HBNB"
+
+if __name__ == "__main__":
+	app.run(host='0.0.0.0', port=5000)
