1b50f5e022b6f88f0b65c87e273d0f9c273dbd3d
theLawrenzo <thelawrenzo1@gmail.com>
2024-08-09 22:56:12 +0000

    Update Flask App

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,32 +1,16 @@
 #!/usr/bin/python3
-# This script starts a Flask application
-
-'''
-Flask application that listens on 0.0.0.0 port 500
+'''Flask web application that is list can be accessed from
+any location
 '''
 
 from flask import Flask
 
-app = Flask("__name__")
+app = Flask(__name__)
 
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+	'''Returns a dynamic string object'''
+	return 'C {}'.format(text)
 
-@app.route('/', strict_slashes=False)
-def hello():
-    '''Returns a string'''
-    return ("Hello HBNB!")
-
-
-@app.route("/hbnb", strict_slashes=False)
-def hbnb():
-    '''Returns a string'''
-    return ("HBNB")
-
-
-@app.route("/c/<text>", strict_slashes=False)
-def cText(text):
-    '''Displays C followed by the value of the text variable'''
-    return "C {}".format(text.replace("_", " "))
-
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=None)
+if __name__ == '__main__':
+	app.run(host='0.0.0.0', port=5000, debug=True)
