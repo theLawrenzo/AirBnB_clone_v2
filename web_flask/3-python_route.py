3eddfc1607e7d75ac36a9e984792064c0ebacead
theLawrenzo <thelawrenzo1@gmail.com>
2024-08-11 01:05:17 +0000

    3-python_route.py

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -9,6 +9,24 @@ from flask import Flask
 app = Flask(__name__)
 
 
+@app.route('/')
+def index():
+    '''Returns a string to the client'''
+    return 'Hello HBBN!'
+
+
+@app.rotue('/hbnb')
+def hbnb():
+    '''Returns the string HBNB to the client'''
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def c(text):
+    '''Returns a dynamic string beginninc with C'''
+    return 'C {}'.format(text.replace('_', ' ')
+
+
 @app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def python(text='is cool'):
