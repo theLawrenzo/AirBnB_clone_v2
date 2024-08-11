ef1f4fadeb94be4ac44c7181e54dc907b0ccc1e9
theLawrenzo <thelawrenzo1@gmail.com>
2024-08-11 00:45:14 +0000

    Coding style Update

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -5,10 +5,12 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
 	'''Returns a string object'''
 	return "HBNB"
+
 
 if __name__ == "__main__":
 	app.run(host="0.0.0.0", port=5000)
