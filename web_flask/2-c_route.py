3040568eac0f79495b1b1f3e0d114e77bf4cad8f
theLawrenzo <thelawrenzo1@gmail.com>
2024-08-11 00:49:27 +0000

    Style update

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -7,20 +7,22 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def index():
-	'''Returns a string HBNB to the client'''
-	return 'Hello HBNB!'
+    '''Returns a string HBNB to the client'''
+    return 'Hello HBNB!'
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-	'''Returns a string HBNB to the client'''
-	return 'HBNB'
+    '''Returns a string HBNB to the client'''
+    return 'HBNB'
 
 @app.route('/c/<text>', strict_slashes=False)
 def c(text):
-	'''Returns a dynamic string object to the client'''
-	return 'C {}'.format(text.replace('_', ' '))
+    '''Returns a dynamic string object to the client'''
+    return 'C {}'.format(text.replace('_', ' '))
+
 
 if __name__ == '__main__':
-	app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
