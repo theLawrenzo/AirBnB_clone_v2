73bfec8afe7a41ab7a11a209a2211df26bcd6f65
theLawrenzo <thelawrenzo1@gmail.com>
2024-08-11 23:31:44 +0000

    Update

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -5,11 +5,16 @@ Application and maps url to approciate page
 '''
 
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 
-@app.route('/number_template/<n>')
+@app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
     '''Returns an html page to the client'''
-    return ''
+    return render_template('5-number_template.html', n=n)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
