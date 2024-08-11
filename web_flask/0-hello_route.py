637f11a78680d2853882a25e981e1520d4f9f5f2
theLawrenzo <thelawrenzo1@gmail.com>
2024-08-11 00:39:41 +0000

    Update

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -9,8 +9,8 @@ app = Flask(__name__)
 
 @app.route("/", strict_slashes=False)
 def index():
-	'''Function that displays the home page'''
-	return "Hello HBNB!"
+    '''Function that displays the home page'''
+    return "Hello HBNB!"
 
 if __name__ == "__main__":
-	app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
