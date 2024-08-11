80034f2ccb7b549df9fd26d176abdf7c39428203
theLawrenzo <thelawrenzo1@gmail.com>
2024-08-11 00:58:36 +0000

    Script update

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 '''
 Starts a Flask web application listening on all host on
 a network interface
@@ -9,4 +8,13 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/python/<text>')
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python(text='is cool'):
+    '''Returns a string to the client'''
+    return 'Pyhton {}'.format(text.replace('_', ' '))
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
