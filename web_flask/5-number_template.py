d92cf587cae349f678ac6914660409bd3edea5f3
theLawrenzo <thelawrenzo1@gmail.com>
2024-08-12 22:28:01 +0000

    PYTHON SCRIPT - Contains a flask web application;

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -13,7 +13,7 @@ app = Flask(__name__)
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
     '''Returns an html page to the client'''
-    return render_template('5-number_template.html', n=n)
+    return render_template('5-number.html', n=n)
 
 
 if __name__ == '__main__':
